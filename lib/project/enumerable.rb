dd75a56b48d178fea5129256064f4644f1df16de
Watson <watson1978@gmail.com>
2014-06-07 21:14:25 +0900

    init

---
+++ lib/project/enumerable.rb
@@ -0,0 +1,46 @@
+class Integer
+
+  def p_times(priority=:default, &block)
+    queue = Dispatch::Queue.concurrent(priority)
+    queue.apply(self, &block)
+  end
+
+end
+
+module Enumerable
+
+  def p_each(priority=nil, &block)
+    ary = self.to_a
+    ary.size.p_times(priority) do |i| 
+      block.call(ary[i])
+    end
+    self
+  end
+
+  def p_each_index(priority=nil, &block)
+    ary = self.to_a
+    ary.size.p_times(priority) do |i|
+      block.call(i)
+    end
+    self
+  end
+
+  def p_each_with_index(priority=nil, &block)
+    ary = self.to_a
+    ary.size.p_times(priority) do |i|
+      block.call(ary[i], i)
+    end
+    self
+  end
+
+  def p_map(priority=nil, &block)
+    ary = self.to_a
+    @p_map_result = Array.new(ary.size)
+    ary.p_each_with_index(priority) do |obj, i|
+      @p_map_result[i] = block.call(obj)
+    end
+    @p_map_result
+  end
+  alias :p_collect :p_map
+
+end
